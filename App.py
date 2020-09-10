a2f0864c4d2784f5b4bc9b885acf1956306ae746
Fabrizio Monge <37080465+fabriziomonge@users.noreply.github.com>
2020-09-10 16:28:27 +0200

    Update App.py

--- App.py
+++ App.py
@@ -152,4 +152,15 @@ if uploaded_file1 is not None and uploaded_file2 is not None:
 
 
     df_definitivo
+    
+    def get_table_download_link(df):
+    """Generates a link allowing the data in a given panda dataframe to be downloaded
+    in:  dataframe
+    out: href string
+    """
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+    href = f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'
+    
+    st.markdown(get_table_download_link(df_definitivo), unsafe_allow_html=True)
 
